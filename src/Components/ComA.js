6e36be7d4710bcd72f4f3fab8291f1152ea09b6d
abdul saeed <abdulsaeed1356@gmail.com>
2023-08-02 13:43:10 +0500

    work done

---
+++ src/Components/ComA.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ComA = () => {
+  return (
+    <div>ComA</div>
+  )
+}
+
+export default ComA
---
+++ src/Components/ComB.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const ComB = () => {
+  return (
+    <div>ComB</div>
+  )
+}
